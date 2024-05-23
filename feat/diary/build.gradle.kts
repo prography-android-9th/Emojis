e4273c3c5a745c7d083f9d6b7216352fec146300
torder-raven <109227292+soyeonLee126@users.noreply.github.com>
2024-05-23 09:46:54 +0900

    Update build.gradle.kts

--- feat/diary/build.gradle.kts
+++ feat/diary/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
+    //todo: 확인받기
+    id("kotlin-parcelize")
 }
 
 android {
@@ -24,20 +26,23 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "11"
     }
 }
 
 dependencies {
-
+    //todo: toml에 어떻게 추가할지 논의해보기
+    implementation("com.slack.circuit:circuit-foundation:0.19.1")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.androidx.material3)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.androidx.appcompat)
 }
